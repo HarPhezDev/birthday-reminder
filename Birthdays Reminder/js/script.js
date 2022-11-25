38084ce45853b32b31087667857a51ed02108eb9
HAR PHEZ DEV <84986174+HarPhezDev@users.noreply.github.com>
2022-11-24 19:09:49 -0600

    Add files via upload

---
+++ Birthdays Reminder/js/script.js
@@ -0,0 +1,9 @@
+
+document.querySelector("#click").onclick = function(){
+    document.querySelector("header").innerHTML = "0 Birthdays Today";
+    document.querySelector("#cont").style.display = "none";
+    document.querySelector("#hero").style.height = "25vh";
+}
+
+
+
